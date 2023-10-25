4b4e64bfdd01040a17b00f9cfef32d630f94a6bf
BhavanaPandey1 <148930249+BhavanaPandey1@users.noreply.github.com>
2023-10-24 22:43:22 -0500

    Update run-mysort.sh

--- run-mysort.sh
+++ run-mysort.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TEST_REPO_URL="https://github.com/viveksaka/PA3"
-TEST_REPO_DIR="viveksaka/PA3"
+TEST_REPO_URL="https://github.com/BhavanaPandey1/Testing_PA3"
+TEST_REPO_DIR="BhavanaPandey1/Testing_PA3"
 
 if [ ! -d $TEST_REPO_DIR ]
 then
@@ -12,5 +12,5 @@ cd $TEST_REPO_DIR
 git fetch & git pull
 cd ..
 
-bash $TEST_REPO_DIR/my-check-submission.sh all
-bash $TEST_REPO_DIR/my-submission-test.sh all
+bash $TEST_REPO_DIR/my-result-check.sh all
+bash $TEST_REPO_DIR/my-result-test.sh all
